05b4129004d843e477bc184cc0b3e7e6e81a46da
Patrick Augusto <79328506+Patrick-Augusto@users.noreply.github.com>
2023-07-19 08:48:28 -0300

    Create StockItem.cs

---
+++ BackEnd/C# Entity Framework/StockItem.cs
@@ -0,0 +1,10 @@
+namespace Domain.Entities
+{
+    public class StockItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
