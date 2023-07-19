53420d3ddbff5edbe0ee81088e131ad70d5d3eba
Patrick Augusto <79328506+Patrick-Augusto@users.noreply.github.com>
2023-07-19 09:17:46 -0300

    Create stock-item-form.component.ts

---
+++ FrontEnd/FrameWork Angular/src/stock-item-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item-form',
+  templateUrl: './stock-item-form.component.html',
+})
+export class StockItemFormComponent {
+  @Output() stockItemAdded = new EventEmitter<any>();
+  name: string;
+  quantity: number;
+  unitPrice: number;
+
+  addStockItem() {
+    if (this.name && this.quantity && this.unitPrice) {
+      const stockItem = {
+        name: this.name,
+        quantity: this.quantity,
+        unitPrice: this.unitPrice,
+      };
+      this.stockItemAdded.emit(stockItem);
+      this.name = '';
+      this.quantity = null;
+      this.unitPrice = null;
+    }
+  }
+}
